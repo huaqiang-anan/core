70450b2aa1a9d1b31a9ecc886742d5e46c7229ba
华强 <wuanghuaqiang@zjup.net>
2016-08-03 18:12:57 +0800

    add core

---
+++ src/main/java/com/mj/core/rxjava/ApiCallback.java
@@ -0,0 +1,15 @@
+package com.mj.core.rxjava;
+
+
+/**
+ * Created by oliver on 16/5/7.
+ */
+public interface ApiCallback<T> {
+
+    void onSuccess(T model);
+
+    void onFailure(int code, String msg);
+
+    void onCompleted();
+
+}
